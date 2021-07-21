ed03bc8ab4e521652afd3767ce39f49670d29a42
Polyane Tuag <pollyportugues@hotmail.com>
2021-07-21 20:37:23 -0300

    Ajustando paddings para o responsivo

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -1,4 +1,13 @@
-import { Box, Button, Divider, Flex, Heading, HStack, SimpleGrid, VStack } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Divider,
+  Flex,
+  Heading,
+  HStack,
+  SimpleGrid,
+  VStack,
+} from "@chakra-ui/react";
 import { Input } from "../../components/Form/Input";
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
@@ -11,20 +20,26 @@ export default function CreateUser() {
       <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
         <Sidebar />
 
-        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
-          <Heading size="lg" fontWeight="normal">Criar usuário</Heading>
+        <Box flex="1" borderRadius={8} bg="gray.800" p={["6", "8"]}>
+          <Heading size="lg" fontWeight="normal">
+            Criar usuário
+          </Heading>
 
           <Divider my="6" borderColor="gray.700" />
 
           <VStack spacing="8">
-            <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+            <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
               <Input name="name" label="Nome completo" />
               <Input name="email" type="email" label="E-mail" />
             </SimpleGrid>
 
-            <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+            <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
               <Input name="password" type="password" label="Senha" />
-              <Input name="password_confirmation" type="password" label="Confirmar senha" />
+              <Input
+                name="password_confirmation"
+                type="password"
+                label="Confirmar senha"
+              />
             </SimpleGrid>
           </VStack>
 
@@ -34,9 +49,8 @@ export default function CreateUser() {
               <Button colorScheme="pink">Salvar</Button>
             </HStack>
           </Flex>
-
         </Box>
       </Flex>
     </Box>
-  )
+  );
 }
