09427cf41ad3372980018970c98dacb6f21d4237
Polyane Tuag <pollyportugues@hotmail.com>
2021-07-30 18:57:35 -0300

    Sinalizando link ativo na navegação

---
+++ src/components/ActiveLink.tsx
@@ -0,0 +1,39 @@
+import Link, { LinkProps } from "next/link";
+import { useRouter } from "next/router";
+import { cloneElement, ReactElement } from "react";
+
+interface ActiveLinkProps extends LinkProps {
+  children: ReactElement;
+  shouldMatchExactHref?: boolean;
+}
+
+export function ActiveLink({
+  children,
+  shouldMatchExactHref,
+  ...rest
+}: ActiveLinkProps) {
+  const { asPath } = useRouter();
+
+  let isActive = false;
+
+  if (shouldMatchExactHref && (asPath === rest.href || asPath === rest.as)) {
+    //verifica se o endereço possui o mesmo path
+    isActive = true;
+  }
+
+  if (
+    //verifica se o endereço começa com o mesmo path
+    !shouldMatchExactHref &&
+    (asPath.startsWith(String(rest.href)) || asPath.startsWith(String(rest.as)))
+  ) {
+    isActive = true;
+  }
+
+  return (
+    <Link {...rest}>
+      {cloneElement(children, {
+        color: isActive ? "pink.500" : "gray.50",
+      })}
+    </Link>
+  );
+}
